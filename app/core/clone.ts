067df4b2b3e0b83a91281b4a6f135bc7bc49d2af
dricha <dricha@microsoft.com>
2016-10-04 14:31:16 -0700

    Updated README.md with the important notice

--- app/core/clone.ts
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) Microsoft Corporation. All Rights Reserved. */
-
-/**
- * This is a simple deep clone operation
- */
-export function clone<T>(sourceObject: T): T {
-    return JSON.parse(JSON.stringify(sourceObject));
-}
